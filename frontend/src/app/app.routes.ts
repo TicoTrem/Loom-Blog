e6ac7e3286f51894102781990b194f4950844eb9
TicoTrem <ticotremblay@gmail.com>
2025-10-04 17:42:48 -0600

    Add new item to navbar, convert css to use new css nesting features

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NewPostComponent } from './new-post/new-post.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'blogpost/:id',
     component: BlogPostComponent
+  },
+  {
+    path: 'new-post',
+    component: NewPostComponent
   }
 ];
